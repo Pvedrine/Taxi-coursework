06c1d5c0d4c0107353663ff318f0445b511725e2
Cedric Tremolosa <cedric.tremolosa@gmail.com>
2017-03-19 04:14:56 +0000

    created method save data into the log file

--- src/log/Log.java
+++ src/log/Log.java
@@ -1,9 +1,11 @@
 package log;
 
+//Here is a Singleton pattern so we get only one instance of the object LogFile.
 public class LogFile {
 
 	   //create an object of SingleObject
 	   private static LogFile instance = new LogFile();
+	   private static String filename = System.getProperty("user.dir"); + "\\log.txt"
 
 	   //make the constructor private so that this class cannot be
 	   //instantiated
@@ -14,7 +16,27 @@ public class LogFile {
 	      return instance;
 	   }
 
-	   public void showMessage(String string){
-	      System.out.println(string);
+	   public void printIntoLogFile(String string){
+		   printIntoFile(string);
+	   }
+	   
+	   private void printIntoFile(String string){
+		   BufferedWriter writer = null;
+		   //We try to write
+		   try {
+			   //Opens the file in append mode
+			   writer = new BufferedWriter(new OutputStreamWriter(
+					   new FileOutputStream(this.filename, true), "utf-8"));
+			   //We add the message to the end of the file
+			   writer.write(string + "\n");
+
+			   writer.close();
+			} 
+			//To catch the IOException, in case there is a problem with the file opening
+			catch (IOException ex) {
+				//We print what the error is
+				ex.printStackTrace();
+				System.exit(1); 
+			}
 	   }
 	}
