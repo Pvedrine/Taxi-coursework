a5628a89acccd3c1db0b6cc57b251e8df5ce3bf0
Cedric Tremolosa <cedric.tremolosa@gmail.com>
2017-03-22 00:11:17 +0000

    Added comments to Prog, Controller classes

--- src/stage2/Controller.java
+++ src/stage2/Controller.java
@@ -1,28 +1,45 @@
 package stage2;
 
+//Useful imports
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+//This class is for the controller module of our MVC pattern
 public class Controller {
 
+	/**
+	 * In this property is stored a reference of the model
+	 */
 	private Model model;
+	
+	/**
+	 * In this property is stored a reference of the view
+	 */
 	private View view;
 	
+	/**
+	 * All parameters constructor for this class
+	 */
 	public Controller(Model model, View view){
 		this.model = model;
 		this.view = view;
+		//Add an event handler from the view to the model
 		this.view.addProcessBookingJourneyListener(new ProcessBookingJourneyController());
 	}
 
-
+	/**
+	 * Event handler, with creation of a new thread, so the GUI is non-blocking
+	 */
 	class ProcessBookingJourneyController  implements ActionListener
 	{
 	    public void actionPerformed(ActionEvent e) 
 	    { 
+	    	//Deactivate the ProcessButton not to be able to click again
 	    	view.disableProcessButton();
+	    	//creathe thread for non blocking GUI
 			Thread thread = new Thread (model);
 			thread.start();
-
+			//Enable the ProcessButton to launch the app again
 			view.enableProcessButton();
 	    }
 	}
--- src/stage2/Program.java
+++ src/stage2/Program.java
@@ -1,13 +1,28 @@
 package stage2;
 
+/**
+ * This is the main class of the app
+ */
 public class Program {
 
+	/**
+	 * This is the main method for the stage 2 of the app
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Model model = new Model(4);   //the model
+		//We create the model 
+		//With for argument number of workers+1
+		//So arg = 4 => 3 workers
+		Model model = new Model(4);
+		
+		//we create the view
         View   view  = new View(model);
+        
+        //we creathe the controller
         @SuppressWarnings("unused")
 		Controller controller = new Controller(model, view);   
+        
+        //The components of the MVC are created, we show the GUI
         view.setVisible(true); 
 	}
 
